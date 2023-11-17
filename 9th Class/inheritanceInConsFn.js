0e645f1123c76cf0fd71aa7c83066491c12fb667
acciof3 <acciof3@gmail.com>
2023-11-17 22:26:47 +0530

    cons fn inheritance 2

--- 9th Class/inheritanceInConsFn.js
+++ 9th Class/inheritanceInConsFn.js
@@ -4,12 +4,16 @@ function BankAccount(name, bankName, balance) {
   this.balance = balance;
 }
 
-function SavingsAccount(name, bankName, balance, cibil) {
-  this.name = name;
-  this.bankName = bankName;
-  this.balance = balance;
-  this.cibil = cibil;
-}
+BankAccount.prototype.addMoney = function (amount) {
+  this.balance += amount;
+};
+
+// function SavingsAccount(name, bankName, balance, cibil) {
+//   this.name = name;
+//   this.bankName = bankName;
+//   this.balance = balance;
+//   this.cibil = cibil;
+// }
 
 function SavingsAccountModified(name, bankName, balance, cibil) {
   this.cibil = cibil;
@@ -18,10 +22,22 @@ function SavingsAccountModified(name, bankName, balance, cibil) {
   BankAccount.call(this, name, bankName, balance);
 }
 
+// earlier this `SavingsAccountModified.prototype` was inherting
+// from Object.prototype but after this line
+// it will inherit from BankAccount.prototype
+SavingsAccountModified.prototype.__proto__ = BankAccount.prototype;
+
 const user1 = new BankAccount("suyash", "janDhan", 100);
-const user2 = new SavingsAccount("naveen", "hdfc", 10000, 780);
+// const user2 = new SavingsAccount("naveen", "hdfc", 10000, 780);
 const user3 = new SavingsAccountModified("vamshi", "axis", 5000, 750);
 
 console.log(user1);
-console.log(user2);
+// console.log(user2);
+console.log(user3);
+
+user1.addMoney(500);
+user3.addMoney(500);
+
+console.log(user1);
+// console.log(user2);
 console.log(user3);
